8600a7f4d2b4122b355b309bb9fdbf9db9b4032d
liamoconnor87 <liamoconnor87@gmail.com>
2020-08-25 14:59:09 +0000

    JS: Initial card match logic added.

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const cards = document.querySelectorAll('.memory-card');
 
-function flipcard(){
-    this.classList.toggle('flip');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    //flip card animation
+    this.classList.add('flip');
+
+    //if hasflippedcard isnt false
+    if (!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        //second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        //do cards match...
+        if (firstCard.dataset.name === secondCard.dataset.name) {
+            //it matches!
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            //no match!
+            setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            }, 1500);
+        }
+    }
 }
 
-cards.forEach(card => card.addEventListener('click', flipcard));
+cards.forEach(card => card.addEventListener('click', flipCard));
