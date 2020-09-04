caaa7ab42d28f96f6096c0efbf9c49ba434e124e
liamoconnor87 <liamoconnor87@gmail.com>
2020-09-04 12:09:41 +0000

    Style: Load screen animation implemented.

---
+++ animations.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+	/* Burger Loading Screen */
+	$(".load img").delay(500).animate({
+		opacity: '1',
+	}, 700)
+	$(".load img").animate({
+		opacity: '0',
+	}, 500)
+	$(".load img").animate({
+		opacity: '1',
+	}, 700)
+	$(".load img").animate({
+		opacity: '0',
+	}, 500)
+	$(".load img").animate({
+		opacity: '1',
+	}, 700)
+	$(".load img").animate({
+		opacity: '0',
+	}, 500)
+	$(".load").delay(4100).animate({
+		opacity: '0',
+	}, 500)
+
+	$(".title").delay(4600).animate({
+		opacity: '1',
+	}, 500)
+
+	$(".game-container").delay(4800).animate({
+		opacity: '1',
+	}, 500)
+})
