c9c77463326fe9231182fb871417df633dcfbf1f
liamoconnor87 <liamoconnor87@gmail.com>
2020-09-09 19:50:33 +0000

    JS: Logic added for other card matches.

--- playtheme.js
+++ playtheme.js
@@ -5,19 +5,74 @@ function play() {
 }
 
 // Mutes sound. 
-var x = document.getElementById("theme");
+var theme = document.getElementById("theme");
 
 function enableMute() { 
-  x.muted = true;
+  theme.muted = true;
 } 
 
 function disableMute() { 
-  x.muted = false;
+  theme.muted = false;
 }
 
 // Triggers when there is a match!
 function soundConfirm() {
     if(document.getElementById("bob") === document.getElementById("bob")) {
-        play();
+        playBob();
+    } else if(document.getElementById("linda") === document.getElementById("linda")) {
+        playLinda();
+    } else if(document.getElementById("tina") === document.getElementById("tina")) {
+        playTina();
+    } else if(document.getElementById("gene") === document.getElementById("gene")) {
+        playGene();
+    } else if(document.getElementById("louise") === document.getElementById("louise")) {
+        playLouise();
+    } else if(document.getElementById("teddy") === document.getElementById("teddy")) {
+        playTeddy();
+    } else if(document.getElementById("marshmellow") === document.getElementById("marshmellow")) {
+        playMarshmellow();
+    } else (document.getElementById("dryap") === document.getElementById("dryap")) {
+        playDrYap();
     }
+}
+
+// Card match sounds
+function playBob() {
+    var audio = document.getElementById("theme");
+    audio.play();
+}
+
+function playLinda() {
+    var audio = document.getElementById("theme");
+    audio.play();
+}
+
+function playTina() {
+    var audio = document.getElementById("theme");
+    audio.play();
+}
+
+function playGene() {
+    var audio = document.getElementById("theme");
+    audio.play();
+}
+
+function playLouise() {
+    var audio = document.getElementById("theme");
+    audio.play();
+}
+
+function playTeddy() {
+    var audio = document.getElementById("theme");
+    audio.play();
+}
+
+function playMarshmellow() {
+    var audio = document.getElementById("theme");
+    audio.play();
+}
+
+function playDrYap() {
+    var audio = document.getElementById("theme");
+    audio.play();
 }
