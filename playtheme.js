24997402299db430697ec7e3526e3c82e7d13eb3
liamoconnor87 <liamoconnor87@gmail.com>
2020-09-09 14:59:27 +0000

    Feat: Initial Mute button works.

--- playtheme.js
+++ playtheme.js
@@ -1,4 +1,14 @@
- function play() {
-        var audio = document.getElementById("theme");
-        audio.play();
-      }
+function play() {
+    var audio = document.getElementById("theme");
+    audio.play();
+}
+
+var x = document.getElementById("theme");
+
+function enableMute() { 
+  x.muted = true;
+} 
+
+function disableMute() { 
+  x.muted = false;
+}
